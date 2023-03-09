2f74bd4a2305d1e18c84116df4e9dbe885df6189
IlyaNov <noil.01@mail.ru>
2023-03-09 19:32:48 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 16455;
+        int bonus = 20;
+        int miles = cost / bonus;
+        System.out.println("Количество бонусных миль:" + miles);
+    }
+}
